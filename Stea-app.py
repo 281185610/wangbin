bcd71d0272d0d99b5a30c2342b19d9ff0cba8a10
281185610 <281185610@qq.com>
2023-06-07 15:43:02 +0800

    Update Stea-app.py
    2

--- Stea-app.py
+++ Stea-app.py
@@ -1,20 +1,30 @@
+这个语法错误是由于字典的键值对最后一个项目后面缺少逗号导致的。
+应该将products字典修改为:
+python
+products = {'产品1':{'purchase_cost':15, 'purchase_margin':20, ...}, 
+            '产品2':{'purchase_cost':20, 'purchase_margin':25, ...},
+            '产品3':{'purchase_cost':25, 'purchase_margin':30, ...},  
+} 
+增加最后一个项目'产品3'后面的逗号 ',' 即可解决这个语法错误。
+完整的代码为:
 python
 import streamlit as st
 
 def ozon_pricing(params):
-    purchase_cost = params['purchase_cost']  
-    purchase_margin = params['purchase_margin']  
-    # ...定价计算逻辑...
-    return price   
+    purchase_cost = params['purchase_cost']
+    purchase_margin = params['purchase_margin']
+    # ...定价计算逻辑...     
+    return price         
 
 # Streamlit实现可视化  
 st.title('Ozon定价计算工具')
 
-products = {'产品1':{'purchase_cost':15, 'purchase_margin':20, ...}, 
+products = {'产品1':{'purchase_cost':15, 'purchase_margin':20, ...},     
             '产品2':{'purchase_cost':20, 'purchase_margin':25, ...},
-            '产品3':{'purchase_cost':25, 'purchase_margin':30, ...}}
+            '产品3':{'purchase_cost':25, 'purchase_margin':30, ...},  
+}
 
-with st.form(key='my_form'):
+with st.form(key='product_form'):
     cols = st.columns(len(products))
     data = []
     for i, (product_name, product_params) in enumerate(products.items()): 
@@ -32,4 +42,4 @@ with st.form(key='my_form'):
         for product in data:  
             product['product_name'] = product_name
             price = ozon_pricing(product)    
-            st.write(f'{product_name}最终定价: ¥{price}') 
+            st.write(f'{product_name}最终定价: ¥{price}')
