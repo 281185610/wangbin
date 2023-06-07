6ca3ce741e9fd19f9a500dfc0a0b3bf4037a2505
281185610 <281185610@qq.com>
2023-06-07 15:26:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+好的,去除`products.csv`文件的依赖是一个比较好的想法。这样可以简化环境配置,更容易实现一个可直接运行的版本。
+这里是去除CSV文件的代码版本:
+python
 import streamlit as st
-import pandas as pd
 
 # 定义定价计算函数    
 def ozon_pricing(params):
@@ -25,37 +27,22 @@ def ozon_pricing(params):
                          (1 - exchange_loss/100) * (1 - goods_loss/100))
     return price
 
-# 读取产品信息
-product_df = pd.read_csv('products.csv')
-
 # Streamlit实现可视化  
 st.title('Ozon定价计算工具')
 
-for index, row in product_df.iterrows():
-    product_name = row['product_name']
-    product_link = row['product_link']
+with st.form(key='product_form'):
+    # 参数输入        
+    params = {}
+    params['purchase_cost'] = st.number_input('采购成本')    
+    params['purchase_margin'] = st.number_input('采购成本利润率%') 
+    params['logistics_fee'] = st.number_input('物流费用') 
+    params['other_fee'] = st.number_input('其他费用')
+    params['fixed_fee'] = st.number_input('固定费用')
+    params['exchange_rate'] = st.number_input('汇率')      
+    # ...其他参数         
+    submitted = st.form_submit_button('提交')
     
-    with st.form(key=f'product_{index}_form'):
-        # 参数输入        
-        params = {}
-        params['purchase_cost'] = st.number_input(f'{product_name}采购成本', 
-                                                 value=row['purchase_cost']) 
-        params['purchase_margin'] = st.number_input(f'{product_name}采购成本利润率%',  
-                                                   value=row['purchase_margin']) 
-        params['logistics_fee'] = st.number_input(f'{product_name}物流费用', 
-                                                 value=row['logistics_fee']) 
-        params['other_fee'] = st.number_input(f'{product_name}其他费用', 
-                                              value=row['other_fee'])                   
-        params['fixed_fee'] = st.number_input(f'{product_name}固定费用', 
-                                              value=row['fixed_fee'])
-        params['exchange_rate'] = st.number_input(f'{product_name}汇率',  
-                                                 value=row['exchange_rate']) 
-        # ...其他参数   
-            
-        submitted = st.form_submit_button(f'提交{product_name}')
-        
-        if submitted: 
-            # 计算结果
-            price = ozon_pricing(params)  
-            st.write(f'{product_name}最终定价: ¥{price}')
-            st.write(f'{product_name}产品链接: {product_link}')  
+    if submitted: 
+        # 计算结果
+        price = ozon_pricing(params)  
+        st.write(f'最终定价: ¥{price}') 
