325e57c1a17428fdb748c6e32838e1f22ba075a9
281185610 <281185610@qq.com>
2023-06-07 16:24:29 +0800

    Update Stea-app.py
    6

--- Stea-app.py
+++ Stea-app.py
@@ -37,14 +37,16 @@ def ozon_pricing(params):
 st.title('Ozon定价计算工具')
 
 products = {
-    '产品1': {'purchase_cost':15, 'purchase_margin':20,  'logistics_fee':10,  
-              'other_fee':5,  'fixed_fee':100, 'exchange_rate':15, 
-             'promotion_discount':10, 'category_commission':8,  
-             'exchange_loss':3,  'goods_loss':1, 'profit_margin':30}, 
-    '产品2': {'purchase_cost':25, 'purchase_margin':25,  'logistics_fee':15,  
-              'other_fee':8,  'fixed_fee':120, 'exchange_rate':18, 
-             'promotion_discount':12, 'category_commission':10,  
-             'exchange_loss':5,  'goods_loss':3, 'profit_margin':35}  
+    '产品1': {'purchase_cost':15,  'purchase_margin':20,  
+              'logistics_fee':10,  'other_fee':5,  
+              'fixed_fee':100, 'exchange_rate':15,  
+              'promotion_discount':10, 'category_commission':8,   
+              'exchange_loss':3,   'goods_loss':1, 'profit_margin':30}, 
+    '产品2': {'purchase_cost':25,  'purchase_margin':25,  
+              'logistics_fee':15,  'other_fee':8,  
+              'fixed_fee':120, 'exchange_rate':18,  
+              'promotion_discount':12,'category_commission':10,    
+              'exchange_loss':5,   'goods_loss':3, 'profit_margin':35}  
 }  
 
 with st.form(key='product_form'): 
@@ -52,6 +54,18 @@ with st.form(key='product_form'):
     data = []
     for product_name, product_params in products.items():
         with cols[0]:        
+            st.write(f'{product_name}定价计算公式:')
+            formula = f'价格 = 采购成本 × (1 + 采购成本利润率{product_params["purchase_margin"]}%) '       
+            formula += f'+ 物流费用{product_params["logistics_fee"]} + 其他费用{product_params["other_fee"]} '
+            formula += f'+ 固定费用{product_params["fixed_fee"]} '
+            formula += f'× 汇率{product_params["exchange_rate"]} '  
+            formula += f'÷ (1 - 促销折扣{product_params["promotion_discount"]}% - '
+            formula += f'类目佣金{product_params["category_commission"]}% - ' 
+            formula += f'汇率损失{product_params["exchange_loss"]}% - '
+            formula += f'货物损失{product_params["goods_loss"]}%) '
+            formula += f'× (1 + 利润率{product_params["profit_margin"]}%)'
+            st.latex(formula)  
+            
             for param_name in product_params:             
                 product_params[param_name] = st.number_input(
                     f'{product_name}{param_name}',
@@ -59,20 +73,4 @@ with st.form(key='product_form'):
                     help=f'请输入{product_name}{param_name}'
                 )
         data.append(product_params) 
-    submitted = st.form_submit_button('提交') 
-        
-    if submitted:
-        results = []
-        for product in data:         
-            product_name =  product.pop('product_name')
-            price, profit = ozon_pricing(product)
-            product['price'] = price
-            product['profit'] = profit 
-            product['product_name'] = product_name
-            results.append(product) 
-            
-        st.write('定价与利润计算结果:')
-        st.write(pd.DataFrame(results))
-            
-        with st.expander('导出结果到Excel'):
-            st.write(pd.DataFrame(results).to_excel())  
+    submitted = st.form_submit_button('提交')
