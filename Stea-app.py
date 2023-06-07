26ca6287f89323b7b0bbe2227a69d45745a7f609
281185610 <281185610@qq.com>
2023-06-07 17:20:46 +0800

    Update Stea-app.py

--- Stea-app.py
+++ Stea-app.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 def ozon_pricing(
     purchase_cost,
-    purchase_margin,
-    profit_margin, 
+    purchase_margin, 
     logistics_fee,
     other_fee, 
     fixed_fee,
@@ -16,11 +15,8 @@ def ozon_pricing(
     # 采购成本加成计算
     cost_plus = purchase_cost * (1 + purchase_margin/100)  
     
-    # 利润计算
-    profit = cost_plus * profit_margin/100  
-    
     # 其他费用计算
-    total_cost = cost_plus + profit + logistics_fee + other_fee + fixed_fee  
+    total_cost = cost_plus + logistics_fee + other_fee + fixed_fee  
 
     # 汇率调整
     total_cost = total_cost * exchange_rate  
@@ -30,29 +26,19 @@ def ozon_pricing(
                          (1 - category_commission/100) *
                          (1 - exchange_loss/100) *  
                          (1 - goods_loss/100))
-    return price, profit
+    formula = f'定价={total_cost} / (1 - {promotion_discount}%折扣) / (1 - {category_commission}%佣金) / (1-{exchange_loss}%汇率损失) / (1-{goods_loss}%货损)'
+    return price, formula
 
-# Streamlit实现可视化
-st.title('Ozon定价与利润计算工具')
+# Streamlit实现可视化  
+st.title('Ozon定价计算工具')  
 
 with st.form(key='my_form'):
-    col1, col2 = st.columns(2)
+    ...
     
-    with col1:
-        ...
-        purchase_margin = st.number_input('采购成本利润率%', value=20) 
-        profit_margin = st.number_input('销售利润率%', value=30)        
-        
-    with col2:
-        ...
-        
-    submitted = st.form_submit_button('提交')
-
-    if submitted:
-        price, profit = ozon_pricing(purchase_cost, purchase_margin,  
-                                    profit_margin,logistics_fee, other_fee, 
-                                     fixed_fee, exchange_rate,  
-                                     promotion_discount,category_commission,
-                                     exchange_loss, goods_loss)
-        st.write(f'最终定价: ¥{price}')  
-        st.write(f'利润: ¥{profit}') 
+if submitted:
+    price, formula = ozon_pricing(purchase_cost, purchase_margin, 
+                                 logistics_fee, other_fee,fixed_fee, 
+                                 exchange_rate,promotion_discount,         
+                                 category_commission,exchange_loss,goods_loss)
+    st.write(f'最终定价: ¥{price}')
+    st.latex(formula)  # 展示计算公式
