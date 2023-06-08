196ceedf4ddbfe4a73161942a2dde5d99e574d99
281185610 <281185610@qq.com>
2023-06-08 11:48:06 +0800

    Update Stea-app.py
    2

--- Stea-app.py
+++ Stea-app.py
@@ -8,34 +8,29 @@ otherFee = 10
 fixedFee = 20
 exchangeRate = 6.5  
 promotionDiscount = 8  
-categoryCommission = 5 
+categoryCommission = 5  
 exchangeLoss = 2     
-goodsLoss = 3    
+goodsLoss = 3 
+company_name = 'Ozon中国'   
 
 def ozonPricing(purchaseCost: float):  
      """Ozon定价计算函数"""  
-     price = purchaseCost * (1 + purchaseMargin/100)  
-     price = price * (1 - promotionDiscount/100)  
-     price = price * exchangeRate * (1 - exchangeLoss/100)
+     # function body......
      
-     profit = price - purchaseCost - logisticsFee - otherFee - fixedFee
-     formula = f'利润 = 定价 - 采购成本 - 物流费 - 其他费用 - 固定费用'
-     return price, formula  
-
-# Streamlit实现可视化  
-st.title('Ozon定价计算工具')  
+# Streamlit实现可视化     
+st.latex(f'定价 = {purchaseCost}*(1 + {purchaseMargin}/100)*(1 - {promotionDiscount}/100)*{exchangeRate}*(1 - {exchangeLoss}/100)')
+st.title(f'{company_name}定价计算工具')  
 
 # 获取用户输入并转换为float类型
 purchaseCost = st.number_input('请输入采购成本', min_value=0.0, max_value=1000000,  
-                               step=0.01, format='%f')   
+                               step=0.01, format='%f')
 
-# 调用函数进行计算
+# 调用函数进行计算  
 if purchaseCost:  
-     price, formula = ozonPricing(purchaseCost)  
-     st.write(f'最终定价: ¥{price}')
-     st.latex(formula)
-     profit = price - purchaseCost - logisticsFee - otherFee - fixedFee  
-     st.write(f'利润: ¥{profit}')
-     st.latex('利润 = 定价 - 采购成本 - 物流费 - 其他费用 - 固定费用')
+     price = purchaseCost * (1 + purchaseMargin/100)  
+     price = price * (1 - promotionDiscount/100)  
+     price = price * exchangeRate * (1 - exchangeLoss/100)  
+     st.write(f'{company_name}最终定价: ¥{price}')  
+     # ......
 else:  
      st.warning('请输入采购成本!')
