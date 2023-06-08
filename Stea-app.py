48b07add5258525f31ed664c7ddbe6155117caea
281185610 <281185610@qq.com>
2023-06-08 10:20:18 +0800

    Update Stea-app.py
    23

--- Stea-app.py
+++ Stea-app.py
@@ -1,38 +1,13 @@
 import streamlit as st
 
 def ozon_pricing(
-    purchase_cost,
-    purchase_margin, 
-    logistics_fee,
-    other_fee, 
-    fixed_fee,
-    exchange_rate, 
-    promotion_discount,
-    category_commission,
-    exchange_loss, 
-    goods_loss 
-):
+    purchase_cost,  
+    ...  
+):  
     if not isinstance(purchase_cost, (int, float)):
         st.warning('请输入数值类型的参数!')
         return
-    # 其他参数也增加类型检查......
-    
-    # 采购成本加成计算
-    cost_plus = purchase_cost * (1 + purchase_margin/100)  
-    
-    # 其他费用计算
-    total_cost = cost_plus + logistics_fee + other_fee + fixed_fee  
-
-    # 汇率调整
-    total_cost = total_cost * exchange_rate  
-
-    # 折扣与损耗计算
-    price = total_cost / ((1 - promotion_discount/100) * 
-                         (1 - category_commission/100) *
-                         (1 - exchange_loss/100) *  
-                         (1 - goods_loss/100))
-    formula = f'定价={total_cost} / (1 - {promotion_discount}%折扣) / (1 - {category_commission}%佣金) / (1-{exchange_loss}%汇率损失) / (1-{goods_loss}%货损)'
-    return price, formula
+    ...
 
 # Streamlit实现可视化  
 st.title('Ozon定价计算工具')  
@@ -45,17 +20,18 @@ purchase_cost = 15
 if isinstance(purchase_cost, (int, float)): 
     price, formula = ozon_pricing(purchase_cost, ...)
     st.write(f'最终定价: ¥{price}')  
-    st.latex(formula)
+    st.latex(formula) 
 else:
-    st.warning('请输入数值类型的参数!')
+    st.warning('请输入数值类型的参数!')  
 
 with st.form(key='my_form'):
-    ...
+    ...  
 if submitted: 
-    purchase_cost = st.number_input('采购成本', value=15) 
+    purchase_cost = st.number_input('采购成本', value=15)
+    purchase_cost = purchase_cost  
     if isinstance(purchase_cost, (int, float)):
         price, formula = ozon_pricing(purchase_cost, ...)  
         st.write(f'最终定价: ¥{price}')
         st.latex(formula)    
     else:  
-        st.warning('请输入数值类型的参数!') 
+        st.warning('请输入数值类型的参数!')
