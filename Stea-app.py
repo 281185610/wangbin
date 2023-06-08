00105c9554583b0dc78b90410bf64d6604becb30
281185610 <281185610@qq.com>
2023-06-08 10:22:33 +0800

    Update Stea-app.py
    4

--- Stea-app.py
+++ Stea-app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
 def ozon_pricing(
-    purchase_cost,  
-
-):  
+    purchase_cost  #采购成本
+): 
     if not isinstance(purchase_cost, (int, float)):
         st.warning('请输入数值类型的参数!')
         return
- 
+    ...
 
 # Streamlit实现可视化  
 st.title('Ozon定价计算工具')  
@@ -17,19 +16,20 @@ purchase_cost = 15
 
 # 调用函数进行计算  
 if isinstance(purchase_cost, (int, float)): 
-    price, formula = ozon_pricing(purchase_cost, ...)
+    price, formula = ozon_pricing(purchase_cost)
     st.write(f'最终定价: ¥{price}')  
-    st.latex(formula) 
+    st.latex(formula)
 else:
-    st.warning('请输入数值类型的参数!')  
+   st.warning('请输入数值类型的参数!')
 
+# Form获取参数   
 with st.form(key='my_form'):
-    ...  
+    ...
 if submitted: 
     purchase_cost = st.number_input('采购成本', value=15)
     purchase_cost = purchase_cost  
     if isinstance(purchase_cost, (int, float)):
-        price, formula = ozon_pricing(purchase_cost, ...)  
+        price, formula = ozon_pricing(purchase_cost)
         st.write(f'最终定价: ¥{price}')
         st.latex(formula)    
     else:  
