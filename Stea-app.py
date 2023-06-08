ca1c0472dfa74c7d811bd46a23896780c8387050
281185610 <281185610@qq.com>
2023-06-08 13:48:04 +0800

    Update Stea-app.py
    23

--- Stea-app.py
+++ Stea-app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 def ozon_pricing(
     purchase_cost,
     purchase_margin,
@@ -23,15 +24,21 @@ def ozon_pricing(
                          (1 - exchange_loss/100) *
                          (1 - goods_loss/100))
     return price
+
 # Streamlit实现可视化
 st.title('Ozon定价计算工具')
+
 # 显示计算公式
-st.write('*计算公式:*')
+
+# 显示计算公式
+st.write('**计算公式:**')
 st.latex(r'''
-price = \frac{total\_cost}{(1 - \frac{promotion\_discount}{100})
-(1 - \frac{category\_commission}{100}) (1 - \frac{exchange\_loss}{100})
-(1 - \frac{goods\_loss}{100})}
+定价 = \frac{总成本}{(1 - \frac{促销折扣}{100}) 
+(1 - \frac{类目佣金}{100}) (1 - \frac{汇率损失}{100})
+(1 - \frac{货物损失}{100})}
 ''')
+
+...
 # 显示输入字段
 with st.form(key='my_form'):
     col1, col2 = st.columns(2)
@@ -44,7 +51,7 @@ with st.form(key='my_form'):
         fixed_fee = st.number_input('固定费用', value=100)
         exchange_rate = st.number_input('汇率', value=15)
         # 显示字段值
-        st.write('*字段值:*')
+        st.write('**字段值:**')
         st.write(f'采购成本: {purchase_cost}')
         st.write(f'采购成本利润率%: {purchase_margin}')
         st.write(f'物流费用: {logistics_fee}')
@@ -57,9 +64,9 @@ with st.form(key='my_form'):
         exchange_loss = st.number_input('汇率损失%', value=3)
         goods_loss = st.number_input('货物损失%', value=1)
         # 显示字段值
-        st.write('*字段值:*')
+        st.write('**字段值:**')
         st.write(f'促销折扣%: {promotion_discount}')
-         st.write(f'类目佣金%: {category_commission}')
+        st.write(f'类目佣金%: {category_commission}')
         st.write(f'汇率损失%: {exchange_loss}')
         st.write(f'货物损失%: {goods_loss}')
     submitted = st.form_submit_button('提交')
@@ -69,5 +76,5 @@ with st.form(key='my_form'):
                              promotion_discount, category_commission,
                              exchange_loss, goods_loss)
         # 显示计算值
-        st.write('*计算值:*')
+        st.write('**计算值:**')
         st.write(f'最终定价: ¥{price:.2f}')
