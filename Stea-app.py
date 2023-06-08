e6bce3975eef7f1c67bc2ae2fa29f861ade4cd9b
281185610 <281185610@qq.com>
2023-06-08 10:36:11 +0800

    Update Stea-app.py
    23

--- Stea-app.py
+++ Stea-app.py
@@ -4,11 +4,12 @@ def ozon_pricing(purchase_cost):
         if not isinstance(purchase_cost, (int, float)):
                  st.warning('请输入数值类型的参数!')
                  return
-    # 其他参数也增加类型检查......
+    # 其他参数也增加类型检查......   
 
-        # 采购成本加成计算
-    cost_plus = purchase_cost * (1 + purchase_margin/100)
+            cost_plus = purchase_cost * (1 + purchase_margin/100)
 
+        # 采购成本加成计算  
+ 
     # 其他费用计算
     total_cost = cost_plus + logistics_fee + other_fee + fixed_fee
 
@@ -23,29 +24,29 @@ def ozon_pricing(purchase_cost):
     formula = f'定价={total_cost} / (1 - {promotion_discount}%折扣) / (1 - {category_commission}%佣金) / (1-{exchange_loss}%汇率损失) / (1-{goods_loss}%货损)'
     return price, formula  
 
-# Streamlit实现可视化  
+# Streamlit实现可视化
 st.title('Ozon定价计算工具')  
 
-# 定义参数初值  
+# 定义参数初值
 purchase_cost = 15  
 
-# 调用函数进行计算  
-if isinstance(purchase_cost, (int, float)): 
+# 调用函数进行计算
+if isinstance(purchase_cost, (int, float)):
     price, formula = ozon_pricing(purchase_cost)
-    st.write(f'最终定价: ¥{price}')  
+    st.write(f'最终定价: ¥{price}')
     st.latex(formula)
 else:
    st.warning('请输入数值类型的参数!')
 
-# Form获取参数   
+# Form获取参数  
 with st.form(key='my_form'):
-    ...
-if submitted: 
+    ...  
+if submitted:
     purchase_cost = st.number_input('采购成本', value=15)
-    purchase_cost = purchase_cost  
+    purchase_cost = purchase_cost
     if isinstance(purchase_cost, (int, float)):
         price, formula = ozon_pricing(purchase_cost)
         st.write(f'最终定价: ¥{price}')
-        st.latex(formula)    
-    else:  
+        st.latex(formula)  
+    else:
         st.warning('请输入数值类型的参数!')
