336b52e2d881d917dd68223cfd845c8703f9c418
281185610 <281185610@qq.com>
2023-06-08 11:12:05 +0800

    Update Stea-app.py
    23

--- Stea-app.py
+++ Stea-app.py
@@ -13,31 +13,23 @@ exchangeLoss = 2     # 汇率损失,百分数
 goodsLoss = 3        # 货损,百分数
 
 def ozonPricing(purchaseCost: float):  
-     """Ozon定价计算函数"""
-     # 参数类型检查......
-     #  function body......  
+     """Ozon定价计算函数"""  
+     # function body...... 
 
-     # 采购成本加成计算
-     costPlus = purchaseCost * (1 + purchaseMargin/100)   
-     
-     # 其他费用计算 
-     totalCost = costPlus + logisticsFee + otherFee + fixedFee  
+# Streamlit实现可视化
+st.title('Ozon定价计算工具')
 
-     # 汇率调整
-     totalCostAfterExchangeRate = totalCost * exchangeRate
-     
-     # 折扣因素提取 
-     discount = 1 - promotionDiscount/100 
-     commission = 1 - categoryCommission/100
-     exchangeLossFactor = 1 - exchangeLoss/100
-     goodsLossFactor = 1 - goodsLoss/100
-     
-     # 价格计算
-     price = totalCostAfterExchangeRate / (discount * commission * 
-                                          exchangeLossFactor *
-                                          goodsLossFactor  )
-     formula = f'定价={totalCostAfterExchangeRate} / 折扣 / 佣金 / 汇率损失 / 货损'
-     return price, formula
+# 获取用户输入并转换为float类型  
+purchaseCost = st.number_input('请输入采购成本', min_value=0.0, max_value=1000000,  
+                               step=0.01, format='%f')  
+
+# 调用函数进行计算 
+if purchaseCost:  
+     price, formula = ozonPricing(purchaseCost)  
+     st.write(f'最终定价: ¥{price}')  
+     st.latex(formula)  
+else:  
+     st.warning('请输入采购成本!')
 
 
 # Streamlit实现可视化 
